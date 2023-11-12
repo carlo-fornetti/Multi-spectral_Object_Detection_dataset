095d9510681459b734a323d067a0cf0a62d991e3
carlo-fornetti <128153748+carlo-fornetti@users.noreply.github.com>
2023-11-12 17:09:06 +0100

    Add files via upload

--- test.py
+++ test.py
@@ -9,14 +9,14 @@ from tqdm.auto import tqdm
 import shutil
 from PIL import Image
 
-sample_list = ['000000', '000022', '000052', '000063', '000501']
+sample_list = ['000000', '000022', '000052', '000063', '000501'] #I select a list of samples in the valid folder
 
 valid_dir= "./valid/"
 
-model = YOLO("./runs/detect/train5/weights/best.pt")
-preds = model.predict(valid_dir, save=True)
+model = YOLO("./runs/detect/train5/weights/best.pt")  #load the weights for the model trained
+preds = model.predict(valid_dir, save=True) #prediction
 
-for sample_name in sample_list:
+for sample_name in sample_list: #print the samples to see the results
     img_path = "./runs/detect/predict/" + sample_name + '.png'
     image = cv2.imread(img_path)
     cv2.imshow('Result: ', image)
--- train.py
+++ train.py
@@ -8,34 +8,34 @@ from tqdm.auto import tqdm
 import shutil
 from PIL import Image
 
-base_dir= Path("./dataset/ir_det_dataset")
+base_dir= Path("./dataset/ir_det_dataset") 
 img_train_path = base_dir /'Images'/'fir'
 label_path = base_dir /'labels'/'fir'
 
-ims = sorted(list(img_train_path.glob('*')))
+ims = sorted(list(img_train_path.glob('*'))) #merge labels and images for yolov8 format
 labels = sorted(list(label_path.glob('*')))
 pairs = list(zip(ims,labels))
 
-train, test = train_test_split(pairs, test_size=0.1,shuffle=True)
+train, test = train_test_split(pairs, test_size=0.1,shuffle=True) #split images in test and train
 
-train_path = Path('train').resolve()
+train_path = Path('train').resolve() #create train and test folders if necessary
 train_path.mkdir(exist_ok=True)
 valid_path = Path('valid').resolve()
 valid_path.mkdir(exist_ok=True)
 
-for t_img, t_lb in tqdm(train):
+for t_img, t_lb in tqdm(train): #save train images in train folder
     im_path = train_path / t_img.name
     lb_path = train_path / t_lb.name
     shutil.copy(t_img,im_path)
     shutil.copy(t_lb,lb_path)
 
-for t_img, t_lb in tqdm(test):
+for t_img, t_lb in tqdm(test): #save test images in test folder
     im_path = valid_path / t_img.name
     lb_path = valid_path / t_lb.name
     shutil.copy(t_img,im_path)
     shutil.copy(t_lb,lb_path)
 
-model = YOLO("yolov8n.pt")
-train = model.train(data="data.yml", epochs=10)
+model = YOLO("yolov8n.pt") #load a model pretrained on COCO datset
+train = model.train(data="data.yml", epochs=10) #train the model with 10 epochs
 
-valid = model.val()
+valid = model.val() #validate the model
